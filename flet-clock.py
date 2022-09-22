8286fddb9f929cf29e5e6c754864912089b200e3
UX Hololeo <helloearth.us@gmail.com>
2022-09-22 15:37:41 -0700

    refactor to Clock class

--- flet-clock.py
+++ flet-clock.py
@@ -1,34 +1,39 @@
-# Flet Clock Widget - Hololeo Labs
+# Flet Clock Demo App - Hololeo Labs
+# imports Clock widget and displays instances
 import flet
-from flet import Page, Text 
+from flet import Page, Column, Row, Container, Text , UserControl, border_radius, padding
 from flet import threading
-import time
-
-def getTime ():
-    #s = time.ctime() # 'Mon Oct 18 13:35:29 2010'
-    s = time.strftime ('%l:%M:%S') 
-    return s
+import time 
+from fletwidgets import Clock
 
 def on_tick (page):
     while page.th.running is True:
-        page.clock.value = getTime()
-        page.update()
+        page.clock1.render()
+        page.clock2.render()
         time.sleep (1)
-def main(page: Page):
+
+def start_tick(page):
+    page.th = threading.Thread (target=on_tick, args=[page], daemon= True)
+    page.th.running = True
+    page.th.start() 
+
+def page_init (page):
     page.title = "Flet Clock"
     page.window_always_on_top = True
     page.window_width = 400 
     page.window_height = 200 
     page.vertical_alignment = "center" 
-    page.horizontal_alignment = "center"  
-    # display clock ui
-    page.clock = Text ("", size=40)
-    page.clock.value = getTime()
-    page.add (page.clock)
-    # start clock thread
-    page.th = threading.Thread (target=on_tick, args=[page], daemon= True)
-    page.th.running = True
-    page.th.start()    
+    page.horizontal_alignment = "center" 
+
+def main(page: Page):
+    page_init (page)
+    # make two clocks
+    page.clock1 = Clock ()
+    page.clock2 = Clock ()
+    page.clock2.text.color = "yellow"
+    page.add (page.clock1, page.clock2 )
+    # start thread to update clocks
+    start_tick (page)      
     page.update()
 
 flet.app(target=main, assets_dir="assets")
